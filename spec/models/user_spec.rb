b8ae2e61627f28de4ce27bb6e69a79f7fcfcc099
CutyDog <silver.soul.818@gmail.com>
2021-08-28 09:34:24 +0900

    use FactoryBot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   #facrory botが存在するかのテストです
-  # it 'has a valid factory bot' do
-  #   expect(build(:user)).to be_valid
-  # end
+  it 'has a valid factory bot' do
+    expect(build(:user)).to be_valid
+  end
 
   describe 'pass validation' do
     it { is_expected.to validate_presence_of(:name) }
